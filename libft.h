a1624fe6e6a5edcbc40458e5e7c0b2387eada04f
kenny <devcore.ru@gmail.com>
2020-04-16 00:37:34 -0700

    add prototype ft_isalnum in libft.h

--- libft.h
+++ libft.h
@@ -10,10 +10,12 @@ void	ft_bzero(void *s, size_t n);
 size_t	ft_strlen(const char *s);
 int		ft_isdigit(int c);
 int		ft_isalpha(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
+int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+
 
 #endif
 
