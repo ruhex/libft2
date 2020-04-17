653aca96fced439e0f53bbc2a99432c7a1c6dcd6
kenny <devcore.ru@gmail.com>
2020-04-16 21:01:50 -0700

    add prototype ft_atoi.c in libft.h

--- libft.h
+++ libft.h
@@ -8,6 +8,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 size_t	ft_strlen(const char *s);
+int		ft_atoi(const char *str);
 int		ft_isdigit(int c);
 int		ft_isalpha(int c);
 int		ft_isalnum(int c);
@@ -15,6 +16,7 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+
 
 
 #endif
