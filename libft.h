3dded08fd37b0d023ddb0419098e7406d53ed943
kenny <devcore.ru@gmail.com>
2020-04-17 23:01:20 -0700

    #include <stdlib.h>

--- libft.h
+++ libft.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #ifndef LIBFT_H
 #define LIBFT_H
@@ -6,6 +7,7 @@
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
 void	ft_bzero(void *s, size_t n);
 size_t	ft_strlen(const char *s);
 int		ft_atoi(const char *str);
